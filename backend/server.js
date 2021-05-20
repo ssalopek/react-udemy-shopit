063608b8d90d271e7f38b25fbad903384a05fdad
ssalopek <ssalopek.hr@gmail.com>
2021-05-20 14:29:40 +0200

    Connection to MongDB and creating product model
    [3.5] Connection to MongoDb using local url
    [4.1] Created Product model using mongoose Schema

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
-const app = require('./app');
-const dotenv = require('dotenv');
+const app = require('./app')
+const connectDatabase = require('./config/database')
 
-//Setting up config file
-dotenv.config({path: 'backend/config/config.env'})
+// Setting up config file
+if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
 
-app.listen(process.env.PORT, ()=>{
+// Connecting to database
+connectDatabase();
+
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
 })
