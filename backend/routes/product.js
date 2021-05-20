e0d0bc52ae4ce96586cb798d032284fd580a2807
ssalopek <ssalopek.hr@gmail.com>
2021-05-20 15:39:30 +0200

    Update and Delete product
    [4.5] Update product by ID
    [4.6] Delete product by ID

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const router = express.Router();
 
-const { getProducts, newProduct, getSingleProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
 
+//GET
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/product/new').post(newProduct);
+//POST
+router.route('/admin/product/new').post(newProduct);
+
+//PUT
+router.route('/admin/product/:id').put(updateProduct);
+
+//DELETE
+router.route('/admin/product/:id').delete(deleteProduct);
 
 module.exports = router;
