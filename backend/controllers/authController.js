a82b54292fcb11842136500334e1b870e2c1a940
Stjepan <ssalopek.hr@gmail.com>
2021-05-24 14:36:32 +0200

    Login User
    [7.4] Login user by comparing tokens (email and password token generated when creating user)

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -26,3 +26,36 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     })
 
 })
+
+//Login user -> /api/v1/login
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+
+    const {email, password} = req.body;
+
+    //Check if email and password is entered by user
+    if(!email || !password){
+        return next(new ErrorHandler('Please enter email and password'));
+    }
+
+    //Find user in database
+    const user = await User.findOne({ email })
+            .select('+password') //because select value of password is by default 'false' (check models/user.js), .select() with + operator is needed
+
+    if(!user){
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
+
+    //Check if password is correct or not
+    const isPasswordMatched = await user.comparePassword(password);
+    if(!isPasswordMatched){
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
+
+    const token = user.getJwtToken();
+
+    res.status(200).json({
+        success: true, 
+        token
+    })
+
+})
--- backend/models/user.js
+++ backend/models/user.js
@@ -51,6 +51,11 @@ userSchema.pre('save', async function(next){ //before saving user do something. 
     this.password = await bcrypt.hash(this.password, 10); //10 is recomended number for salt value
 })
 
+//Compare user password
+userSchema.methods.comparePassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password); //comapre given password 'enteredPasword' with given hash
+}
+
 //Return JSON Web Token 
 userSchema.methods.getJwtToken = function () {
     return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
