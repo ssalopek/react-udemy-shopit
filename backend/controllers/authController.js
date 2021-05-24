cbcae7d8d90ef470eff56996bde90dce044d3465
Stjepan <ssalopek.hr@gmail.com>
2021-05-24 14:47:03 +0200

    Creating cookie by sending JSON web token
    [7.5]

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken');
 
 //Register user -> /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -18,12 +19,7 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
         }
     })
 
-    const token = user.getJwtToken();
-
-    res.status(201).json({
-        success: true, 
-        token
-    })
+    sendToken(user, 200, res)
 
 })
 
@@ -51,11 +47,6 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
         return next(new ErrorHandler('Invalid Email or Password', 401));
     }
 
-    const token = user.getJwtToken();
-
-    res.status(200).json({
-        success: true, 
-        token
-    })
+    sendToken(user, 200, res);
 
 })
