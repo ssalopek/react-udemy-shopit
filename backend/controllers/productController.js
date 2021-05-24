ba395c27c757536985ade959fc46bc19338694b8
Stjepan <ssalopek.hr@gmail.com>
2021-05-24 23:33:44 +0200

    Create review
    [10.1]

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -83,3 +83,42 @@ exports.deleteProduct = catchAsyncErrors(async(req, res, next) => {
         message: "Product is deleted"
     })
 })
+
+//Create new review -> /api/v1/review
+exports.createReview = catchAsyncErrors(async(req, res, next) => {
+
+    const {rating, comment, productId} = req.body;
+
+    const review = {
+        user: req.user._id,
+        name: req.user.name,
+        rating: Number(rating),
+        comment
+    }
+
+    const product = await Product.findById(productId);
+
+    const isReviewed = product.reviews.find(
+        r => r.user.toString() === req.user._id.toString()
+    )
+    if(isReviewed){
+        product.reviews.forEach(review => {
+            if(review.user.toString() === req.user._id.toString()){ //if same user is rating
+                review.comment = comment; //replace new comment
+                review.rating = rating; //replace new rating
+            }
+        })
+    } else {
+        product.reviews.push(review); //push new review
+        product.numOfReviews = product.reviews.length; //update number of reviews
+    }
+
+    product.ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+
+    await product.save({ validateBeforeSave: false});
+
+    res.status(200).json({
+        success: true
+    })
+
+})
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router();
 
-const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
+const { 
+    getProducts, 
+    newProduct, 
+    getSingleProduct, 
+    updateProduct, 
+    deleteProduct, 
+    createReview } = require('../controllers/productController')
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
@@ -17,5 +23,8 @@ router.route('/admin/product/:id').put(isAuthenticatedUser, authorizeRoles('admi
 
 //DELETE
 router.route('/admin/product/:id').delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
+
+//Create review
+router.route('/review').put(isAuthenticatedUser, createReview);
 
 module.exports = router;
