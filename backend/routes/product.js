8bbc9b9b4a0b20eca23fc1e51b918a264f20491e
Stjepan <ssalopek.hr@gmail.com>
2021-05-24 15:27:07 +0200

    Authorizing roles and pesmissions
    [7.8]

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,19 +3,19 @@ const router = express.Router();
 
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
 
-const { isAuthenticatedUser } = require('../middlewares/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 //GET
 router.route('/products').get(isAuthenticatedUser, getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
 //POST
-router.route('/admin/product/new').post(newProduct);
+router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
 
 //PUT
-router.route('/admin/product/:id').put(updateProduct);
+router.route('/admin/product/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct);
 
 //DELETE
-router.route('/admin/product/:id').delete(deleteProduct);
+router.route('/admin/product/:id').delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
 
 module.exports = router;
