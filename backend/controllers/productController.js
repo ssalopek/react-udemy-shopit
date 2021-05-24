7908679efe78ff68392f2a332dbd9389c41d3a8d
Stjepan <ssalopek.hr@gmail.com>
2021-05-24 15:38:44 +0200

    Generating forgot password token
    [7.10] Generate token and encrypt it with crypto lib. Also set token expire time

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,6 +5,8 @@ const APIFeatures = require('../utils/apiFeatures');
 
 //Create new product that will be passed to url like /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
+    req.body.user = req.user.id;
+
     const product = await Product.create(req.body);
 
     res.status(201).json({
