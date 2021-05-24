de4d5710c2b309db021c1dffba3ebb9b030b9463
Stjepan <ssalopek.hr@gmail.com>
2021-05-24 14:17:04 +0200

    JSON web token
    [7.3] Generating JSON web token when creating new user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -18,9 +18,11 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
         }
     })
 
+    const token = user.getJwtToken();
+
     res.status(201).json({
         success: true, 
-        user
+        token
     })
 
 })
