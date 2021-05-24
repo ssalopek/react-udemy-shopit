35a4aef695a7d3ee5ce24b56d50d01123ad5dc47
Stjepan <ssalopek.hr@gmail.com>
2021-05-24 22:35:31 +0200

    Get single and user order by ID
    [9.3]

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const {newOrder} = require('../controllers/orderController');
+const {
+    newOrder, 
+    getSingleOrder, 
+    myOrders} = require('../controllers/orderController');
 
 const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
 
 //Create new order
 router.route('/order/new').post(isAuthenticatedUser, newOrder);
+
+//Get single order
+router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
+
+//Get users order (my orders)
+router.route('/orders/me').get(isAuthenticatedUser, myOrders);
 
 module.exports = router;
