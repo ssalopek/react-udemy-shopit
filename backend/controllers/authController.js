71b52bc92c4823e02773b446bc4c0fc427caf59c
Stjepan <ssalopek.hr@gmail.com>
2021-05-24 15:14:12 +0200

    Revert "Logout"
    This reverts commit 0a6cc90b9955e24e3971d3e8312b78591d17bdf3.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -50,18 +50,3 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     sendToken(user, 200, res);
 
 })
-
-//Logout user -> /api/v1/logout
-exports.logout = catchAsyncErrors(async (req, res, next) => {
-
-    //Clear cookies
-    res.cookie('token', null, {
-        expires: new Date(Date.now()),
-        httpOnly: true
-    })
-
-    res.status(200).json({
-        succes: true,
-        message: 'Logged out'
-    })
-})
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {registerUser, loginUser, logout} = require('../controllers/authController');
+const {registerUser, loginUser} = require('../controllers/authController');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
-
-router.route('/logout').get(logout);
 
 module.exports = router;
