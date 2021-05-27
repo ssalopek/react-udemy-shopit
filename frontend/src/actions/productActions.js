ded047b4da9c507f00854548f5d217355c45845a
Stjepan <ssalopek.hr@gmail.com>
2021-05-28 00:32:19 +0200

    Reducer and action for product details
    [12.8]

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,34 +1,58 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { 
-    ALL_PRODUCTS_REQUEST, 
-    ALL_PRODUCTS_SUCCESS,
-    ALL_PRODUCTS_FAIL,
-    CLEAR_ERRORS } from '../constants/productConstants';
+import {
+  ALL_PRODUCTS_REQUEST,
+  ALL_PRODUCTS_SUCCESS,
+  ALL_PRODUCTS_FAIL,
+  CLEAR_ERRORS,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+} from "../constants/productConstants";
 
+//Get all products
 export const getProducts = () => async (dispatch) => {
-    try {
-        
-        dispatch({ type: ALL_PRODUCTS_REQUEST})
+  try {
+    dispatch({ type: ALL_PRODUCTS_REQUEST });
 
-        const { data } = await axios.get('/api/v1/products');
+    const { data } = await axios.get("/api/v1/products");
 
-        dispatch({
-            type: ALL_PRODUCTS_SUCCESS,
-            payload: data
-        })
+    dispatch({
+      type: ALL_PRODUCTS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ALL_PRODUCTS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
-    } catch (error) {
-        dispatch({
-            type: ALL_PRODUCTS_FAIL,
-            payload: error.response.data.message
-        })
-    }
-}
+//Get single product by ID (product that is clicked on)
+export const getProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: PRODUCT_DETAILS_REQUEST,
+    });
+
+    const { data } = await axios.get(`/api/v1/product/${id}`);
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data.product,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
 //Clear errors
 export const clearErrors = () => async (dispatch) => {
-    dispatch({
-        type: CLEAR_ERRORS
-    })
-}
+  dispatch({
+    type: CLEAR_ERRORS,
+  });
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { productsReducer } from './reducers/productReducers';
+import { productsReducer, productDetailsReducer } from './reducers/productReducers';
 
 const reducer = combineReducers({
-    products: productsReducer
+    products: productsReducer,
+    productDetails: productDetailsReducer
 })
 
 //contains all data I want to put into the state, before loading application
