79dd6923a93cd0daf85446700ef77f38d783e779
Stjepan <ssalopek.hr@gmail.com>
2021-05-29 15:43:07 +0200

    Filter by price
    [13.3] rc-slider package

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -11,11 +11,13 @@ import {
 } from "../constants/productConstants";
 
 //Get all products
-export const getProducts = (keyword = '', currentPage = 1) => async (dispatch) => {
+export const getProducts = (keyword = '', currentPage = 1, price) => async (dispatch) => {
   try {
     dispatch({ type: ALL_PRODUCTS_REQUEST });
 
-    const { data } = await axios.get(`/api/v1/products?keyword=${keyword}&page=${currentPage}`);
+    let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}`;
+
+    const { data } = await axios.get(link);
 
     dispatch({
       type: ALL_PRODUCTS_SUCCESS,
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,6 +2,8 @@ import React, { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useAlert } from "react-alert";
 import Pagination from "react-js-pagination";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 import MetaData from "./layout/MetaData";
 import Product from "./product/Product";
@@ -9,8 +11,12 @@ import Loader from "./layout/Loader";
 
 import { getProducts } from "../actions/productActions";
 
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
+
 const Home = ({ match }) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([1, 1000]);
 
   const dispatch = useDispatch();
   const alert = useAlert();
@@ -24,8 +30,8 @@ const Home = ({ match }) => {
     if (error) {
       return alert.error(error);
     }
-    dispatch(getProducts(keyword, currentPage));
-  }, [dispatch, alert, error, keyword, currentPage]);
+    dispatch(getProducts(keyword, currentPage, price));
+  }, [dispatch, alert, error, keyword, currentPage, price]);
 
   function setCurrentPageNo(pageNumber) {
     setCurrentPage(pageNumber);
@@ -41,10 +47,44 @@ const Home = ({ match }) => {
           <h1 id="products_heading">Latest Products</h1>
           <section id="products" className="container mt-5">
             <div className="row">
-              {products &&
+              {keyword ? (
+                <Fragment>
+                  <div className="col-6 col-md-3 mt-5 mb-5">
+                    <div className="px-5">
+                      <Range
+                        marks={{
+                          1: `$1`,
+                          1000: `$1000`,
+                        }}
+                        min={1}
+                        max={1000}
+                        defaultValue={[1, 1000]}
+                        tipFormatter={(value) => `$${value}`}
+                        tipProps={{
+                          placement: "top",
+                          visible: true,
+                        }}
+                        value={price}
+                        onChange={(price) => setPrice(price)}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-6 col-md-9">
+                    <div className="row">
+                      {products &&
+                        products.map((product) => (
+                          <Product key={product.id} product={product} col={4} />
+                        ))}
+                    </div>
+                  </div>
+                </Fragment>
+              ) : (
+                products &&
                 products.map((product) => (
-                  <Product key={product.id} product={product} />
-                ))}
+                  <Product key={product.id} product={product} col={3} />
+                ))
+              )}
             </div>
           </section>
           {resultsPerPage <= productsCount && (
@@ -63,7 +103,6 @@ const Home = ({ match }) => {
               />
             </div>
           )}
-          
         </Fragment>
       )}
     </Fragment>
