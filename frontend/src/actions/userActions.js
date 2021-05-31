5373248e343482f2dd850e529b709190a89b39a0
Stjepan <ssalopek.hr@gmail.com>
2021-05-31 19:00:15 +0200

    Register reducer/action
    [14.3]

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -5,9 +5,12 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   CLEAR_ERRORS,
+  REGISTER_REQUEST,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
 } from "../constants/userConstants";
 
-//Login
+//Login user
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({ type: LOGIN_REQUEST });
@@ -36,9 +39,37 @@ export const login = (email, password) => async (dispatch) => {
   }
 };
 
+//Register user
+export const register = (userData) => async (dispatch) => {
+  try {
+    dispatch({
+      type: REGISTER_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    const { data } = await axios.post("/api/v1/register", userData, config);
+
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: data.user,
+    });
+    
+  } catch (error) {
+    dispatch({
+      type: REGISTER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
 //Clear errors
 export const clearErrors = () => async (dispatch) => {
-    dispatch({
-      type: CLEAR_ERRORS,
-    });
-  };
+  dispatch({
+    type: CLEAR_ERRORS,
+  });
+};
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -2,23 +2,30 @@ import {
     LOGIN_REQUEST,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
-    CLEAR_ERRORS
+    CLEAR_ERRORS,
+    REGISTER_REQUEST,
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
 } from '../constants/userConstants';
 
 export const userReducer = (state = { user: {} }, action) => {
     switch(action.type){
         case LOGIN_REQUEST:
+        case REGISTER_REQUEST:
             return{
                 loading: true,
                 isAuthenticated: false
             }
         case LOGIN_SUCCESS:
+        case REGISTER_SUCCESS:
             return{
+                ...state,
                 loading: false,
                 isAuthenticated: true,
                 user: action.payload
             }
         case LOGIN_FAIL:
+        case REGISTER_FAIL:
             return{
                 ...state,
                 loading: false,
