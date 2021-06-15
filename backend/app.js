c2de01be4c79b468d7042a53882299ea14e38f19
Stjepan <ssalopek.hr@gmail.com>
2021-06-15 20:33:18 +0200

    Stripe backend
    [16.4]
    stripe package

--- backend/app.js
+++ backend/app.js
@@ -16,10 +16,12 @@ app.use(fileUpload());
 const products = require("./routes/product");
 const auth = require("./routes/auth");
 const order = require("./routes/order");
+const payment = require("./routes/payment");
 
 app.use("/api/v1", products);
 app.use("/api/v1", auth);
 app.use("/api/v1", order);
+app.use("/api/v1", payment);
 
 if (process.env.NODE_ENV === "PRODUCTION") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
