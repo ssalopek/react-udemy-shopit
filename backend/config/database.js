5f495e5f1f0d337a1397dade37c0f9f00d02369e
Stjepan <ssalopek.hr@gmail.com>
2021-06-20 22:48:17 +0200

    Live MongoDB setup and connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_LOCAL_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    }).then(con => {
-        console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
+  mongoose
+    .connect(process.env.DB_URI2, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
     })
-}
+    .then((con) => {
+      console.log(
+        `MongoDB Database connected with HOST: ${con.connection.host}`
+      );
+    });
+};
 
-module.exports = connectDatabase
+module.exports = connectDatabase;
--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,6 +1,7 @@
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const Stripe = require("stripe"); //(process.env.STRIPE_SECRET_KEY);
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Process stripe payments   =>   /api/v1/payment/process
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
